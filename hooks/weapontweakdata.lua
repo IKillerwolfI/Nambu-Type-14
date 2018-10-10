a71e93f84ee2d04251dad3450613a3a1a18203ff
IKillerwolfI <killerwolf.no1@gmail.com>
2018-10-10 22:36:33 +0200

    Cursed gun
    - Changed the firing sounds
    - "Fixed" the bolt flying out of the gun on the last round
    - Tried to fix mag issue with no success

--- hooks/weapontweakdata.lua
+++ hooks/weapontweakdata.lua
@@ -1,3 +1,12 @@
 Hooks:PostHook( WeaponTweakData, "init", "Nambu14Init", function(self)
-
+if ( self.nambu14 ) then	
+--[[
+	self:SetupAttachmentPoint( "nambu14", {
+		name = "a_m",
+		base_a_obj = "a_m",
+		position = Vector3( 0, 5, 0 ), 
+        rotation = Rotation( 0, 0, 0 )
+		})
+	--]]
+end
 end)
