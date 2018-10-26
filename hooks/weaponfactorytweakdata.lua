b5f7e158bfdf427d3427e00ca4742b3cafb1f3e4
IKillerwolfI <killerwolf.no1@gmail.com>
2018-10-26 05:53:27 +0200

    Oh right this exists
    -Everything

--- hooks/weaponfactorytweakdata.lua
+++ hooks/weaponfactorytweakdata.lua
@@ -1,3 +1,22 @@
 Hooks:PostHook( WeaponFactoryTweakData, "init", "Nambu14Init", function(self)
 
+
+	local nambu14_exts = {
+		"wpn_fps_upg_ns_pis_medium",
+		"wpn_fps_upg_ns_pis_small",
+		"wpn_fps_upg_ns_pis_large_kac",
+		"wpn_fps_upg_ns_pis_medium_gem",
+		"wpn_fps_upg_ns_pis_medium_slim",
+		"wpn_fps_upg_ns_pis_jungle",
+		"wpn_fps_upg_ns_pis_meatgrinder",
+		"wpn_fps_upg_ns_pis_ipsccomp",
+		"wpn_fps_upg_pis_ns_flash",
+		"wpn_fps_upg_ns_pis_large"
+	}
+
+	for i, part_id in ipairs(nambu14_exts) do
+		self.wpn_fps_pis_nambu14.override[part_id] = {a_obj = "a_ns", parent = "barrel"}
+		self.wpn_fps_pis_x_nambu14.override[part_id] = {a_obj = "a_ns", parent = "barrel"}
+	end
+
 end )
--- hooks/weapontweakdata.lua
+++ hooks/weapontweakdata.lua
@@ -1,12 +1,30 @@
 Hooks:PostHook( WeaponTweakData, "init", "Nambu14Init", function(self)
 if ( self.nambu14 ) then	
---[[
 	self:SetupAttachmentPoint( "nambu14", {
-		name = "a_m",
-		base_a_obj = "a_m",
-		position = Vector3( 0, 5, 0 ), 
+		name = "a_rowaw", --fuck overkill fuck overkil fuck overkill
+		base_a_obj = "a_body",
+		position = Vector3( 0, 4.6, -4.3 ), 
         rotation = Rotation( 0, 0, 0 )
 		})
-	--]]
+	self:SetupAttachmentPoint( "nambu14", {
+		name = "a_ns",
+		base_a_obj = "a_ns",
+		position = Vector3( 0, 1.2, -0.55 ), 
+        rotation = Rotation( 0, 0, 0 )
+		})
+end
+if ( self.x_nambu14 ) then	
+	self:SetupAttachmentPoint( "x_nambu14", {
+		name = "a_rowaw", --fuck overkill fuck overkil fuck overkill
+		base_a_obj = "a_body",
+		position = Vector3( 0, 4.6, -4.3 ), 
+        rotation = Rotation( 0, 0, 0 )
+		})
+	self:SetupAttachmentPoint( "x_nambu14", {
+		name = "a_ns",
+		base_a_obj = "a_ns",
+		position = Vector3( 0, 1.2, -0.55 ), 
+        rotation = Rotation( 0, 0, 0 )
+		})
 end
 end)
